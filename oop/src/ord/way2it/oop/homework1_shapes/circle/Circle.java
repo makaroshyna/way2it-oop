4fa9f0a1d88bf7ef164ced74af0ab281c2548544
makaroshyna <katya23225@gmail.com>
2022-05-26 02:05:00 +0300

    Implemented the second task of homework - Circle

---
+++ oop/src/ord/way2it/oop/homework1_shapes/circle/Circle.java
@@ -0,0 +1,18 @@
+package ord.way2it.oop.homework1_shapes.circle;
+
+public class Circle {
+
+    public double radius;
+
+    public Circle() {
+        radius = 5;
+    }
+
+    public double getSquare() {
+        return Math.PI * radius * radius;
+    }
+
+    public double getLength() {
+        return 2 * Math.PI * radius;
+    }
+}
---
+++ oop/src/ord/way2it/oop/homework1_shapes/circle/Main.java
@@ -0,0 +1,10 @@
+package ord.way2it.oop.homework1_shapes.circle;
+
+public class Main extends Circle {
+
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.printf("Square of circle = %.3f\n", circle.getSquare());
+        System.out.printf("Length of circle = %.3f", circle.getLength());
+    }
+}
